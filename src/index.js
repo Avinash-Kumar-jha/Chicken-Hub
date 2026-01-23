ea421893c6f5b47da4a32b2019895b7b57ff24bf
Avinash-Kumar-jha <avinashaj004@gmail.com>
2026-01-23 09:47:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const corsOptions = {
     const allowedOrigins = process.env.ALLOWED_ORIGINS
       ? process.env.ALLOWED_ORIGINS.split(',')
       : [
-          'http://localhost:3000',
+          'https://tangerine-conkies-59a07c.netlify.app',
           'http://localhost:3001',
           'http://localhost:5173',
           'http://localhost:5174',

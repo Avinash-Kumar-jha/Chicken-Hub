083e022f7cf3f0f1ee15d05526d9c8c6a175f739
Avinash-Kumar-jha <avinashaj004@gmail.com>
2026-01-23 11:20:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const corsOptions = {
     const allowedOrigins = process.env.ALLOWED_ORIGINS
       ? process.env.ALLOWED_ORIGINS.split(',')
       : [
-          'https://tangerine-conkies-59a07c.netlify.app',
+          'https://remarkable-sawine-e76010.netlify.app',
           'http://localhost:3001',
           'http://localhost:5173',
           'http://localhost:5174',
